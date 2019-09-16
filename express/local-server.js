a063f8734ef4106355ec38fec0bdff9e883458b0
Martin Eldredge <martin.eldredge1@gmail.com>
2019-09-16 09:25:30 -0600

    please

---
+++ express/local-server.js
@@ -0,0 +1,20 @@
+'use strict';
+const express = require('express');
+const path = require('path');
+const serverless = require('serverless-http');
+const app = express();
+
+// viewed at http://localhost:3000
+
+app.use(express.static(__dirname))
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/buy', function(req, res) {
+    res.sendFile(path.join(__dirname + '/buy.html'))
+});
+
+app.listen(3000);
+
+module.exports.handler = serverless(app);
